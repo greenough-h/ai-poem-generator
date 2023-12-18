af3799bc07a6abebc883b671b335f07d00ff96fb
Hayley <37635523+greenough-h@users.noreply.github.com>
2023-12-18 11:28:39 -0500

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let apiKey = "t0cee57ed010o387a24333e4fba6d54e";
+  let topic = document.querySelector(".poem-topic");
+  let prompt = `Write a short poem about ${topic.value}`;
+  let context =
+    "You ae a famous short poem writed who enjoys creating visuals with your words. Please create a poem less than 8 lines long in basic HTML";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "testing",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -11,11 +22,3 @@ function generatePoem(event) {
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
-
-function poemAI() {
-  let apiKey = "t0cee57ed010o387a24333e4fba6d54e";
-  let prompt = "Write a short poem";
-  let context =
-    "You ae a famous short poem writed who enjoys creating visuals with your words. Please create a poem less than 8 lines long";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-}
