a89ff0082f2e389e00fbccd2ff36a22019ed07a4
Hayley <37635523+greenough-h@users.noreply.github.com>
2023-12-18 18:09:49 -0500

    Updated UX features
    example subject input and generating poem message

--- src/index.js
+++ src/index.js
@@ -5,8 +5,19 @@ function generatePoem(event) {
   let topic = document.querySelector(".poem-topic");
   let prompt = `Write a short poem about ${topic.value}`;
   let context =
-    "You ae a famous short poem writed who enjoys creating visuals with your words. Please create a poem less than 8 lines long in basic HTML";
+    "You ae a famous short poem writer who enjoys creating visuals with your words. Please create a poem less than 8 lines long in basic HTML. Each line in the poem should have its own line in the HTML. Please also sign the poem as `<strong> SheCodes AI </strong>` at the end of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemSpace = document.querySelector(".poem");
+  poemSpace.classList.remove("hidden");
+
+  new Typewriter("#poem", {
+    strings: `⏳ Generating Poem about ${topic.value}`,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+    loop: true,
+  });
 
   axios.get(apiUrl).then(displayPoem);
 }
